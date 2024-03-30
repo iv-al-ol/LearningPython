f8e2945d1f39e8411b678808f08a2980ea53c4cc
iv_al_ol village <mr.upon@yandex.ru>
2024-03-30 18:45:48 +0300

    Добавлены поиск в ширину и А*

--- generic_search.py
+++ generic_search.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from heapq import heappop, heappush
 from typing import TypeVar, Iterable, Sequence, \
                     Generic, List, Callable, Set, \
                     Deque, Dict, Any, Optional
@@ -45,17 +46,17 @@ def dfs(initial: T, goal_test: Callable[[T], bool],
     # продолжаем, пока есть что просматривать
     while not frontier.empty:
         current_node: Node[T] = frontier.pop()
-    current_state: T = current_node.state
-    # если мы нашли искомое, заканчиваем
-    if goal_test(current_state):
-        return current_node
-    # проверяем, куда можно двинуться дальше и что мы еще не исследовали
-    for child in successors(current_state):
-        # пропустить состояния, которые уже исследовали
-        if child in explored:
-            continue
-        explored.add(child)
-        frontier.push(Node(child, current_node))
+        current_state: T = current_node.state
+        # если мы нашли искомое, заканчиваем
+        if goal_test(current_state):
+            return current_node
+        # проверяем, куда можно двинуться дальше и что мы еще не исследовали
+        for child in successors(current_state):
+            # пропустить состояния, которые уже исследовали
+            if child in explored:
+                continue
+            explored.add(child)
+            frontier.push(Node(child, current_node))
     return None # все проверили, пути к целевой точке не нашли
 
 def node_to_path(node: Node[T]) -> List[T]:
@@ -66,3 +67,86 @@ def node_to_path(node: Node[T]) -> List[T]:
         path.append(node.state)
     path.reverse()
     return path
+
+class Queue(Generic[T]):
+    def __init__(self) -> None:
+        self._container: Deque[T] = Deque()
+        
+    @property
+    def empty(self) -> bool:
+        return not self._container # не рабно True для пустого контейнера
+    
+    def push(self, item: T) -> None:
+        self._container.append(item)
+    
+    def pop(self) -> T:
+        return self._container.popleft() # FIFO
+    
+    def __repr__(self) -> str:
+        return repr(self._container)
+
+def bfs(initial: T, goal_test: Callable[[T], bool], 
+        successors: Callable[[T], List[T]]) -> Optional[Node[T]]:
+    # froпtier - это то, что мы только собираемся проверить
+    frontier: Queue[Node[T]] = Queue()
+    frontier.push(Node(initial, None))
+    # expLored - это то, что уже проверено
+    explored: Set[T] = {initial}
+    
+    # продолжаем, пока есть что проверять
+    while not frontier.empty:
+        current_node: Node[T] = frontier.pop()
+        current_state: T = current_node.state
+        # если мы нашли искомое, то процесс закончен
+        if goal_test(current_state):
+            return current_node
+        # ищем неисследованные ячейки, в которые можно перейти
+        for child in successors(current_state):
+            if child in explored: #пропустить состояния, которые уже исследовали
+                continue
+            explored.add(child)
+            frontier.push(Node(child, current_node))
+    return None # все проверили, пути к целевой точке не нашли
+
+class PriorityQueue(Generic[T]):
+    def __init__(self) -> None:
+        self._container: List[T] = []
+        
+    @property
+    def empty(self) -> bool:
+        return not self._container #не Тrие для пустого контейнера
+    
+    def push(self, item: T) -> None:
+        heappush(self._container, item) #поместить в очередь по приоритету
+
+    def pop(self) -> T:
+        return heappop(self._container) # извлечь по приоритету
+    
+    def _repr_(self) -> str:
+        return repr(self._container)
+
+def astar(initial: T, goal_test: Callable[[T], bool], 
+          successors: Callable[[T], List[T]], 
+          heuristic: Callable[[T], float]) -> Optional[Node[T]]:
+    # froпtier - то, куда мы хотим двигаться
+    frontier: PriorityQueue[Node[T]] = PriorityQueue()
+    frontier.push(Node(initial, None, 0.0, heuristic(initial)))
+    # expLored - то, что мы уже просмотрели
+    explored: Dict[T, float] = {initial: 0.0}
+    # продолжаем, пока есть что просматривать
+    while not frontier.empty:
+        current_node: Node[T] = frontier.pop()
+        current_state: T = current_node.state
+        # если цель найдена, мы закончили
+        if goal_test(current_state):
+            return current_node
+        # проверяем, в какую из неисследованных ячеек направиться
+        for child in successors(current_state):
+            new_cost: float = current_node.cost + 1
+            # 1 - для сетки, для более сложных приложений здесь
+            # должна быть функция затрат
+            if child not in explored or explored[child] > new_cost:
+                explored[child] = new_cost
+                frontier.push(Node(child, current_node, new_cost,
+                heuristic(child)))
+    return None # все проверили, пути к целевой точке не нашли
--- maze.py
+++ maze.py
@@ -2,24 +2,24 @@ from enum import Enum
 from typing import List, NamedTuple, Callable, Optional
 import random
 from math import sqrt
-from generic_search import dfs, node_to_path, Node#, bfs, astar
+from generic_search import dfs, node_to_path, Node, bfs, astar
 
 class Cell(str, Enum):
-    EMPTY = "."
-    BLOCKED = "Х"
-    START = "S"
-    GOAL = "G"
-    РАТН = "*"
+    EMPTY = ".."
+    BLOCKED = "##"
+    START = ">>"
+    GOAL = "<<"
+    РАТН = "**"
     
 class MazeLocation(NamedTuple):
     row: int
     column: int
 
 class Maze:
-    def __init__(self, rows: int = 10, columns: int = 10, 
+    def __init__(self, rows: int = 15, columns: int = 15, 
                  sparseness: float = 0.2, 
                  start: MazeLocation = MazeLocation(0, 0), 
-                 goal: MazeLocation = MazeLocation(9, 9)) -> None:
+                 goal: MazeLocation = MazeLocation(14, 14)) -> None:
         # инициализация базовых переменных экземпляра
         self._rows: int = rows
         self._columns: int = columns
@@ -68,7 +68,7 @@ class Maze:
 
     def mark(self, path: List[MazeLocation]):
         for maze_location in path:
-            self._grid[maze_location.row][maze_location.column] = Cell.PATH
+            self._grid[maze_location.row][maze_location.column] = Cell.РАТН
         self._grid[self.start.row][self.start.column] = Cell.START
         self._grid[self.goal.row][self.goal.column] = Cell.GOAL
 
@@ -77,6 +77,21 @@ class Maze:
             self._grid[maze_location.row][maze_location.column] = Cell.EMPTY
         self._grid[self.start.row][self.start.column] = Cell.START
         self._grid[self.goal.row][self.goal.column] = Cell.GOAL
+        
+def euclidean_distance(goal: MazeLocation) -> Callable[[MazeLocation], float]:
+    def distance(ml: MazeLocation) -> float:
+        xdist: int = ml.column - goal.column
+        ydist: int = ml.row - goal.row
+        return sqrt((xdist * xdist) + (ydist * ydist))
+    return distance
+
+def manhattan_distance(goal: MazeLocation) -> Callable[[MazeLocation], float]:
+    def distance(ml: MazeLocation) -> float:
+        xdist: int = abs(ml.column - goal.column)
+        ydist: int = abs(ml.row - goal.row)
+        return (xdist + ydist)
+    return distance
+            
 
 if __name__ == "__main__":
     # Тестирование DFS
@@ -93,3 +108,26 @@ if __name__ == "__main__":
         m.mark(path1)
         print(m)
         m.clear(path1)
+
+    solution2: Optional[Node[MazeLocation]] = bfs(m.start,
+                                                  m.goal_test,
+                                                  m.successors)
+    if solution2 is None:
+        print("Нет решения для поиска в ширину!")
+    else:
+        path2: List[MazeLocation] = node_to_path(solution2)
+        m.mark(path2)
+        print(m)
+        m.clear(path2)
+
+    distance: Callable[[MazeLocation], float] = manhattan_distance(m.goal)
+    solutionЗ: Optional[Node[MazeLocation]] = astar(m.start, 
+                                                    m.goal_test, 
+                                                    m.successors, distance)
+    if solutionЗ is None:
+        print("Нет решения для A*!")
+    else:
+        pathЗ: List[MazeLocation] = node_to_path(solutionЗ)
+        m.mark(pathЗ)
+        print(m)
+    
